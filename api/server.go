89704a9ed99f46886b8eac5bde2dab406a2de677
steven victor <chikodi543@gmail.com>
2019-09-07 15:30:52 +0100

    Development commit

---
+++ api/server.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/victorsteven/fullstack/api/controllers"
+	"github.com/victorsteven/fullstack/api/seed"
+)
+
+var server = controllers.Server{}
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("sad .env file found")
+	}
+}
+
+func Run() {
+
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error getting env, %v", err)
+	} else {
+		fmt.Println("We are getting the env values")
+	}
+
+	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+
+	seed.Load(server.DB)
+
+	server.Run(":8080")
+
+}
